cebc0444edf1b1d6038f072b78a859381d7ca497
VenomVendor <info@VenomVendor.com>
2017-04-09 23:50:30 +0530

    initial commit.

---
+++ app/src/main/java/com/venomvendor/app/wordpress/core/WPApplication.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright(c) 2017 - Present, VenomVendor
+ * Licensed under GNU AGPLv3
+ * Contact : info@VenomVendor.com
+ */
+
+package com.venomvendor.app.wordpress.core;
+
+import android.app.Application;
+import android.util.Log;
+
+public class WPApplication extends Application {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Log.d("WPApplication", stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
---
+++ app/src/test/java/com/venomvendor/app/wordpress/ExampleUnitTest.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright(c) 2017 - Present, VenomVendor
+ * Licensed under GNU AGPLv3
+ * Contact : info@VenomVendor.com
+ */
+
+package com.venomvendor.app.wordpress;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
