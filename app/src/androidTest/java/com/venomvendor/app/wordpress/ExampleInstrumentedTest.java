75bafd7154770a733dd23513ebfe121c855f4ed1
VenomVendor <info@VenomVendor.com>
2017-12-17 22:45:20 +0530

    Pending commit.

--- app/src/androidTest/java/com/venomvendor/app/wordpress/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/venomvendor/app/wordpress/ExampleInstrumentedTest.java
@@ -9,6 +9,7 @@ package com.venomvendor.app.wordpress;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
--- app/src/main/java/com/venomvendor/app/wordpress/core/WPApplication.java
+++ app/src/main/java/com/venomvendor/app/wordpress/core/WPApplication.java
@@ -13,7 +13,7 @@ public class WPApplication extends Application {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("wp-android");
     }
 
     @Override
